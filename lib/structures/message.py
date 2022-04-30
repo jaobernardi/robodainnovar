7978ed93cebb31905c8b02704aeb946848167f7b
João Lucas Bernardi <joao_bernardi@outlook.com>
2022-04-29 22:27:37 -0300

    Message object

---
+++ lib/structures/message.py
@@ -0,0 +1,29 @@
+from base64 import b64encode
+import mimetypes
+import os
+
+class Message():
+    def __init__(self, whatsapp, api_return):
+        self.__dict__.update(api_return['message'])
+        self.__dict__.update(api_return['user'])
+        self.whatsapp = whatsapp
+    
+    def seen(self):
+        self.whatsapp.seen_message(self.chatID)
+    
+    def reply(self, msg, qoute=True, file=None):
+        options = {}
+        if qoute:
+            options["quotedMessageId"] = self.id
+        
+        if file:
+            file = os.path.abspath(file)
+            with open(file, "rb") as f:
+                data = b64encode(f.read())
+            options['attachment'] = {
+                "data": data.decode("utf-8"),
+                "mimetype": mimetypes.guess_type(os.path.abspath(file))[0],
+                "filename": os.path.basename(file)
+            }
+
+        self.whatsapp.send_message(self.chatID, msg, options)
