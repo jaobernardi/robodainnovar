efb5866fee93e25a59cb217c3c21a3eb6cd222a8
João Lucas Bernardi <joao_bernardi@outlook.com>
2022-06-11 09:48:45 -0300

    Refactoring atending calls

--- handlers/atending_call.py
+++ handlers/atending_call.py
@@ -1,3 +1,4 @@
+import logging
 from pyding import on
 from lib.structures import User
 from lib import config
@@ -5,81 +6,47 @@ from datetime import datetime
 from . import messages
 
 
+logger = logging.getLogger(__name__)
+
+
 @on("menu_atending_call")
 def atending_call(event, carryoption, user: User):
     selector = carryoption.split("::")
     whatsapp = user.whatsapp
-    atendees = {k: {'user': User(v, f"{v['number']}@c.us", k, whatsapp), 'cities': v['cities']} for k, v in config.get_contact()['financeiro'].items()}
+
+
+    city, department, operation = selector
     
-    
-    match selector:
-        case [city, "Financeiro", operation]:
-            if city == 'outside': city = 'Outra cidade'
-            print(city, atendees)
-            for operator_name, operator_info in atendees.items():
+    logger.debug(f'Calling an operator for {city} with option {operation} in the department {department}')
 
-                print(city, operator_info['cities'])
-                if city in operator_info['cities']:
-                    operator = operator_info['user']
-                    print(operator.id)
-                    now = datetime.now()
-                    messages.TEMPORARY_BLOCKS.append(operator.phonenumber)
-                    operator.send_message((
-                        f"📥 — Requisição de atendimento. (Financeiro)\n"
-                        f"Olá {operator_name}! 👋\n"
-                        f"Hoje às {now.hour}h{f'{now.minute}min' if now.minute > 0 else ' '}, "
-                        f"{user.name} solicitou um atendimento {f'referente à _{operation}_ ' if operation != 'other' else ''}"
-                        f"na região de {city}. *Aqui está o contato:*"    
-                    ))
-                    operator.send_message("", contact=user)
-        case [city, "Comercial", operation]:
-            if city == 'outside': city = 'Outra cidade'
-            
-            operator = User(config.get_contact()['comercial']['number'], f"{config.get_contact()['comercial']['number']}@c.us", whatsapp=whatsapp)
-            #operator = user
+    if department in config.get_atendings():
+        department_options = config.get_atendings()[department]
+
+        if city in department_options:
+            logger.debug(f'Getting operators via the city')
+            option_atendings = department_options[city]
+        elif operation in department_options:
+            logger.debug(f'Getting operators via the option')
+            option_atendings = department_options[operation]
+        elif '*' in department_options:
+            logger.debug(f'Getting operators via the default')
+            option_atendings = department_options['*']
+        else:
+            logger.debug(f'No operator found.')
+            return
+        
+        for atending in option_atendings:
+            logger.debug(f'Contacting operator {atending["name"]}')
+            operator = User(atending['number'], f"{atending['number']}@c.us", atending['name'], whatsapp)
             now = datetime.now()
-            messages.TEMPORARY_BLOCKS.append(operator.phonenumber)
             operator.send_message((
-                f"📥 — Requisição de atendimento. (Comercial)\n"
-                f"Olá 👋\n"
+                f"📥 — Requisição de atendimento. ({department})\n"
+                f"Olá {atending['name']}! 👋\n"
                 f"Hoje às {now.hour}h{f'{now.minute}min' if now.minute > 0 else ' '}, "
                 f"{user.name} solicitou um atendimento {f'referente à _{operation}_ ' if operation != 'other' else ''}"
                 f"na região de {city}. *Aqui está o contato:*"    
             ))
             operator.send_message("", contact=user)
-        
-        case [city, "Engenharia", operation]:
-            if city == 'outside': city = 'Outra cidade'
+            logger.debug(f'Finished contacting.')
 
-            operator = User(config.get_contact()['engenharia'][operation]['number'], f"{config.get_contact()['engenharia'][operation]['number']}@c.us", whatsapp=whatsapp)
-            #operator = user
-            now = datetime.now()
-            messages.TEMPORARY_BLOCKS.append(operator.phonenumber)
-            operator.send_message((
-                f"📥 — Requisição de atendimento. (Engenharia)\n"
-                f"Olá 👋\n"
-                f"Hoje às {now.hour}h{f'{now.minute}min' if now.minute > 0 else ' '}, "
-                f"{user.name} solicitou um atendimento {f'referente à _{operation}_ ' if operation != 'other' else ''}"
-                f"na região de {city}. *Aqui está o contato:*"    
-            ))
-            #messages.temporary_blocks.append(operator.phonenumber)
-            operator.send_message("", contact=user)
-        
-        case [city, "Atendimento Geral", operation]:
-            if city == 'outside': city = 'Outra cidade'
-
-            operator = User(config.get_contact()['atendimento geral']['number'], f"{config.get_contact()['atendimento geral']['number']}@c.us", whatsapp=whatsapp)
-            #operator = user
-            now = datetime.now()
-            messages.TEMPORARY_BLOCKS.append(operator.phonenumber)
-            operator.send_message((
-                f"📥 — Requisição de atendimento. (Atendimento Geral)\n"
-                f"Olá 👋\n"
-                f"Hoje às {now.hour}h{f'{now.minute}min' if now.minute > 0 else ' '}, "
-                f"{user.name} solicitou um atendimento {f'referente à _{operation}_ ' if operation != 'other' else ''}"
-                f"na região de {city}. *Aqui está o contato:*"    
-            ))
-            #messages.temporary_blocks.append(operator.phonenumber)
-            operator.send_message("", contact=user)
-        
-        
+    return
