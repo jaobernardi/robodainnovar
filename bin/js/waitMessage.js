f8a89b5c539c22ccd2c9b8f59c3d3e16cfd78d28
João Lucas Bernardi <joao_bernardi@outlook.com>
2022-06-17 17:22:03 -0300

    `Scripts` — Revert changes

--- bin/js/waitMessage.js
+++ bin/js/waitMessage.js
@@ -13,7 +13,7 @@ function handle_message(message) {
             "user": {
                 "phonenumber": message.from.user,
                 "id": message.from._serialized,
-                "name": message.chat.contact.__x_notifyName ? message.chat.contact.__x_notifyName : (message.chat.contact.__x_displayName ? message.chat.contact.__x_displayName : '')
+                "name": message.chat.contact.__x_notifyName ? message.chat.contact.__x_notifyName : ''
             },
             "message": {
                 "type": message.type,
