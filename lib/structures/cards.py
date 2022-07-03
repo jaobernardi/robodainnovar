5cb61c0e07b0190708c5f9d9f9d25fb6e582b57e
João Lucas Bernardi <joao_bernardi@outlook.com>
2022-07-03 05:04:57 -0300

    `Cards` — Add disabled key

--- lib/structures/cards.py
+++ lib/structures/cards.py
@@ -18,9 +18,10 @@ class Card():
         self.name = data['name']
         self.action = CardAction(data['type'])
         self.data = data['data']
+        self.disabled = data['disabled']
         self.invoke_actions = data['invoke']
 
     def call(self, **extras):
-        event = pyding.call("card_call", action=self.action, data=self.data, card=self, **extras)
+        event = pyding.call("card_call", action=self.action, data=self.data, card=self, cancellable=True,**extras)
         rtr_message = (self.invoke_actions['call'] if 'call' in self.invoke_actions else '').format(card=self)
         return rtr_message, event
