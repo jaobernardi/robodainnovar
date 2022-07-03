98841b5c6e8271ca2fc655a6d159708c0d25e9c6
João Lucas Bernardi <joao_bernardi@outlook.com>
2022-07-03 05:03:03 -0300

    `Cards` — Add recipient list override

--- handlers/broadcast_cards.py
+++ handlers/broadcast_cards.py
@@ -3,13 +3,18 @@ from time import sleep
 from pyding import on
 
 from lib.structures import CardAction, User
+from lib.whatsapp import Whatsapp
 
 logger = logging.getLogger(__name__)
 
 @on("card_call", action=CardAction.BROADCAST)
 def broadcast_cards(event, action, data, card, **extras):
-    whatsapp = extras['whatsapp']
-    recipients: list[User] = [User.no_id(**d, whatsapp=whatsapp) for d in data['recipients']]
+    recipients: list[User]
+    whatsapp: Whatsapp = extras['whatsapp']
+    if 'recipient_override' in extras:
+        recipients = [extras['recipient_override']]
+    else:
+        recipients = [User.no_id(**d, whatsapp=whatsapp) for d in data['recipients']]
     logger.info(f"Recieved broadcast invoke for '{card.name}' with {len(recipients)} recipients ")
 
     count = 0
