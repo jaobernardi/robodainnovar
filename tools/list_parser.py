4ea49536e47346e09d68a501d9e11bfc8f9fc990
Jão <joao_bernardi@outlook.com>
2022-09-24 14:39:58 -0300

    `Tools` - Fix file encoding

--- tools/list_parser.py
+++ tools/list_parser.py
@@ -4,12 +4,15 @@ import json
 import csv
 from phone_parser import number_parser
 from tabulate import tabulate
+import logging
 
-def read_file(file):
+logger = logging.getLogger(__name__)
+
+def read_file(file, splitter=';'):
     data = ""
     while bit := file.read(1):
         if bit == "\n":
-            yield data.split(";")
+            yield data.split(splitter)
             data = ""
             continue
         data += bit
@@ -18,8 +21,10 @@ def main():
     data = []
     indexes = {"names": [], "phones": []}
     while True:
+        logger.info("Adding new file.")
         path = input("File path: ")
-        file = read_file(open(path, 'r'))
+        split = input("Split char: ")
+        file = read_file(open(path, 'r', encoding='utf-8'), split)
         header = next(file)
         print(tabulate([header], headers=[i for i in range(len(header))]))
         name_clm = int(input("Name column index: "))
@@ -27,10 +32,12 @@ def main():
 
         for line in file:
             if line[name_clm] in indexes['names']:
+                logger.warn(f"{line[name_clm]} skipped since it was already in the index.")
                 continue
             indexes['names'].append(line[name_clm])
             number = number_parser(line[number_clm])
             if number in indexes['phones'] or not number:
+                logger.warn(f"{line[number_clm]} {'is invalid' if not number else 'was already in the index.'}")
                 continue
             indexes['phones'].append(number)
             data.append({"name": line[name_clm], "number": number})
@@ -38,12 +45,13 @@ def main():
         option = input("[S]ave current data, [A]dd more files, [P]review current data: ")
         match option.lower():
             case 's':
-                with open("output.json", "w") as file:
+                with open("output.json", "w", encoding='utf-8') as file:
                     file.write(json.dumps(data, indent=4))
                 break
             case 'p':
-                with open("preview.json", "w") as file:
+                with open("preview.json", "w", encoding='utf-8') as file:
                     file.write(json.dumps(data, indent=4))
 
 if __name__ == "__main__":
+    logging.basicConfig()
     main()
