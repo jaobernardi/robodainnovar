1a50365c05b2b6c0e956817eabc5e528a78faf4d
Jão <joao_bernardi@outlook.com>
2022-09-24 14:40:43 -0300

    `Cards` - Add effects

--- lib/structures/cards.py
+++ lib/structures/cards.py
@@ -3,9 +3,46 @@ from json import load
 import os
 import pyding
 
+from lib.structures.user import User, Menu
+
 
 class CardAction(Enum):
     BROADCAST = auto()
+
+class CardEffectType(Enum):
+    SETMENU = auto()
+
+class CardEffect():
+    def __init__(self, type, extra):
+        self.type = type
+        self.extra = extra
+    
+    def apply(self, user: User):
+        match self.type:
+            case CardEffectType.SETMENU:
+                user.menu = Menu(self.extra['name'], self.extra['carryOption'])
+                if user.whatsapp:
+                    user.send_message(user.menu.menu_string())
+        
+
+class CardEffects():
+    def __init__(self, effects : list[CardEffect]):
+        self.effects = effects
+
+    def apply(self, *args, **kwargs):
+        for effect in self.effects:
+            effect.apply(*args, **kwargs)
+
+    @classmethod
+    def from_json(cls, data):
+        effects:list[CardEffect] = []
+        for effect in data:
+            effects.append(CardEffect(
+                                CardEffectType(effect["type"]),
+                                effect["data"]
+                            )
+                        )
+        return cls(effects)
 
 
 class Card():
@@ -20,6 +57,7 @@ class Card():
         self.data = data['data']
         self.disabled = data['disabled']
         self.invoke_actions = data['invoke']
+        self.effects: CardEffects = CardEffects.from_json(data['effects']) if 'effects' in data else CardEffects([])
 
     def call(self, **extras):
         event = pyding.call("card_call", action=self.action, data=self.data, card=self, cancellable=True,**extras)
