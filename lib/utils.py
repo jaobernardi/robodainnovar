34f43440aaf677f996ca5d881b234356f77c563f
Jão <joao_bernardi@outlook.com>
2022-09-24 14:41:50 -0300

    `Utils` - Add AnnotationDefaults

--- lib/utils.py
+++ lib/utils.py
@@ -2,17 +2,23 @@ import logging
 import os
 import sys
 import traceback
+from itertools import zip_longest
+from typing import Any, ValuesView
 
 from tendo import singleton
 
 logger = logging.getLogger(__name__)
-
+logger.addHandler(
+                    logging.FileHandler(
+                        'logs/'+'utils.log', encoding="utf-8"
+                    )
+                )
 import inspect
 
 def aquire_lock() -> singleton.SingleInstance:
     return singleton.SingleInstance()
 
-class InitializeArguments:
+class AnnotationDefaults:
     """
     Automaticly casts the items in a function's args annotation
     """
@@ -21,8 +27,21 @@ class InitializeArguments:
 
     @property
     def arguments(self):
-        sig_values = list(inspect.signature(self.function).parameters.values())
+        sig_values : list[inspect.ValuesView[inspect.Parameter]] = list(inspect.signature(self.function).parameters.values())
         return sig_values
+
+    @staticmethod
+    def define_value(arg, value):
+        output: Any = value
+        if arg.annotation not in [arg.empty, None]:
+            # If there is a annotation on the function                
+            if not value:
+                # create an instance of the annotation
+                output = arg.annotation()
+            elif not isinstance(value, arg.annotation):
+                # cast into the annotation
+                output = arg.annotation(value)
+        return output
 
     def __call__(self, *args, **kwargs):
         # Default arguments to None
@@ -30,10 +49,11 @@ class InitializeArguments:
 
         # Build input keyword arguments
         for arg_value, arg in zip(args, self.arguments):
-            input_args[arg.name] = arg.annotation(arg_value) if arg.annotation not in [arg.empty, None] else arg_value
-        
-        for arg_name, arg_value, arg in zip(kwargs.keys(), kwargs.values(), self.arguments):
-            input_args[arg_name] = arg.annotation(arg_value) if arg.annotation not in [arg.empty, None] else arg_value
+            input_args[arg.name] = self.define_value(arg, arg_value)
+
+        for arg_name, arg_value, arg in zip_longest(kwargs.keys(), kwargs.values(), self.arguments):
+            if not arg_name: break
+            input_args[arg_name] = self.define_value(arg, arg_value)
 
         # Call the function
         return self.function(**input_args)
@@ -53,7 +73,7 @@ def load_handlers():
                 logger.error(f"Failed to load handler {handler}")
                 traceback.print_exc(file=sys.stdout)
 
-@InitializeArguments
+@AnnotationDefaults
 def parse_number(number: str):
     country_code, ddd, first, second = "", "", "", ""
     index = 0
